27a96f27d4e5fa0610366e69d2f810aa49ee09ad
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2020-09-21 11:53:52 -0600

    Se separo el diseño en base Se agrego css Se ve en detalle una publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
+    path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
 ]
