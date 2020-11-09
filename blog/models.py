1705ce45e6bfcde4fc62bc823f5c7c96d7465958
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2020-11-09 11:39:32 -0600

    error en el tamaño

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=500)
     texto = models.TextField()
     fecha_creacion = models.DateTimeField('Creado',
                 default=timezone.now)
