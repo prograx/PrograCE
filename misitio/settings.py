b1a917af779f6f146c6b4b179cc60939a53909fc
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2020-11-13 17:35:14 -0600

    Se tiene config diferente para local y para remoto

--- misitio/settings.py
+++ misitio/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
            'NAME': 'github_actions',
